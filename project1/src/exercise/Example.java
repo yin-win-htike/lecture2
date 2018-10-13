69a733bd924e11d0ba223354cc903ea6c5ed5afe
Yin Win Htike <yinwinhtikewp@gmail.com>
2018-10-13 14:54:12 +0630

    reading char function

--- project1/src/exercise/Example.java
+++ project1/src/exercise/Example.java
@@ -1,32 +1,46 @@
 package exercise;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 public class Example {
 	public static void main(String[] args) {
-		readOneByte();
-		
+		readOneChar();
+
 	}
-	
-	
-	private static void  readOneByte(){
+
+	private static void readOneByte() {
 
 		try {
-		InputStream input = new FileInputStream("C:\\javaclass\\project1\\bytes.txt");
+			InputStream input = new FileInputStream("C:\\javaclass\\lecture2\\project1\\bytes.txt");
 
-		int intVal;
-		while ((intVal = input.read()) >=0){
-		byte byteVal = (byte) intVal;
+			int intVal;
+			while ((intVal = input.read()) >= 0) {
+				byte byteVal = (byte) intVal;
 
-		System.out.print(byteVal);
+				System.out.print(byteVal);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		}catch (IOException e){
-		e.printStackTrace();
+	}
+
+	private static void readOneChar() {
+		try {
+			Reader reader = new FileReader("C:\\javaclass\\lecture2\\project1\\char.txt");
+			int intVal;
+
+			while ((intVal = reader.read()) >= 0) {
+				char ch = (char) intVal;
+				System.out.print(ch);
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		}
+	}
+
 }
-
-
-
